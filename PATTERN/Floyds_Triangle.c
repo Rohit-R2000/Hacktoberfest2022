75fa2fb111f46a389414ccb47f7b271892c10a83
Rohit Rajput <83547290+Rohit-R2000@users.noreply.github.com>
2022-10-27 23:23:26 +0530

    added comments to file Floyds_Triangle

--- PATTERN/Floyds_Triangle.c
+++ PATTERN/Floyds_Triangle.c
@@ -13,13 +13,15 @@ CODE :
 int main() {
    int rows, i, j, number = 1;
    printf("Enter the number of rows: ");
-   scanf("%d", &rows);
-   for (i = 1; i <= rows; i++) {
-      for (j = 1; j <= i; ++j) {
-         printf("%d ", number);
+   scanf("%d", &rows);//taking input to get the total rows that should be printed.
+   for (i = 1; i <= rows; i++)  // this for loop define the rows and check rows condition
+   {
+      for (j = 1; j <= i; ++j)// this for loop check j should be less than equal to i and print the data. 
+      {
+         printf("%d ", number);// print the number  
          ++number;
       }
-      printf("\n");
+      printf("\n");//to start from next line
    }
    return 0;
 }
